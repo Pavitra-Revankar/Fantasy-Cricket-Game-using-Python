a102ec922b9e8cb9f1684e0c4dd68dcb1655ffc0
unknown <g.pavitra7@gmail.com>
2023-06-15 20:42:06 +0530

    Created a new repository and added files to it

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='FantasyCricket', version='1.0', description='A package to evalute points.',
+url='#',
+      author='pavitra',
+author_email='g.pavitra7@gmail.com',
+      license='MIT',
+      packages=['FantasyCricket'],
+zip_safe=False)
